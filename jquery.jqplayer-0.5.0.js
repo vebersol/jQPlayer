e1bdd9d819039c4ad29137ba06b7e357e50e3972
vebersol <vebersol@gmail.com>
2013-04-23 00:44:58 -0300

    fix problem on internet explorer

--- jquery.jqplayer-0.5.0.js
+++ jquery.jqplayer-0.5.0.js
@@ -368,7 +368,6 @@ var options;
 		},
 
 		createFlashElement: function () {
-			this.video = $('<object type="application/x-shockwave-flash" width="100%" height="100%"></object>').attr('data', this.options.fallbackOptions.movie);
 			var params = {
 				"allowScriptAccess": "always",
 				"movie": this.options.fallbackOptions.movie,
@@ -381,7 +380,11 @@ var options;
 				"video": this.getVideoSource()
 			};
 
-			this.video.append(this.addParams(params, flashvars));
+			var fallbackHTML = '<object type="application/x-shockwave-flash" width="100%" height="100%" data="' + this.options.fallbackOptions.movie + '">';
+			fallbackHTML += this.addParams(params, flashvars);
+			fallbackHTML += '</object>';
+
+			this.video = $(fallbackHTML);
 
 			$(this.selector).append(this.video);
 		},
