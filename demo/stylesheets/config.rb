861f12e6e59d4c714900c1384142952461971f91
Vinícius Ebersol <vebersol@gmail.com>
2013-06-28 17:55:16 -0300

    change layouts

--- demo/stylesheets/config.rb
+++ demo/stylesheets/config.rb
@@ -29,7 +29,7 @@ http_generated_images_path = "../../img"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets=false
